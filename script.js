852332dce38d757bca9fdb179efe33a2f72d8298
ashruti773-ops <ashruti773@gmail.com>
2026-03-06 10:27:43 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
-function calculateCI() {
+function checkNumber(){
 
-let P = document.getElementById("principal").value;
-let R = document.getElementById("rate").value;
-let T = document.getElementById("time").value;
-let N = document.getElementById("n").value;
+let num = document.getElementById("number").value;
 
-P = Number(P);
-R = Number(R);
-T = Number(T);
-N = Number(N);
+num = Number(num);
 
-let amount = P * Math.pow((1 + (R / (100 * N))), (N * T));
-let compoundInterest = amount - P;
-
-console.log("Compound Interest:", compoundInterest);
-
-document.getElementById("result").innerText =
-"Compound Interest = " + compoundInterest.toFixed(2);
+if(num % 2 === 0){
+console.log("The number is even");
+document.getElementById("result").innerText = "The number is EVEN";
+}
+else{
+console.log("The number is odd");
+document.getElementById("result").innerText = "The number is ODD";
+}
 
 }
